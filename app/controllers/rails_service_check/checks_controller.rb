95cb292da0b189f7fbf8eb5e1dd88c901ae177dc
thickpaddy <mwoods@online.ie>
2012-03-08 18:10:34 +0000

    Allow requests for specific service checks

--- app/controllers/rails_service_check/checks_controller.rb
+++ app/controllers/rails_service_check/checks_controller.rb
@@ -1,5 +1,6 @@
 module RailsServiceCheck
   class ChecksController < ActionController::Base
+
     def all
       RailsServiceCheck.checks.each do |label, check|
         begin
@@ -10,5 +11,19 @@ module RailsServiceCheck
       end
       render :text => "ok"
     end
+
+    def method_missing(m, *args)
+      if RailsServiceCheck.checks.include?(m)
+        begin
+          RailsServiceCheck.checks[m].run
+        rescue
+          render :text => "#{m.to_s.humanize}: #{$!.message}", :status => 500 and return
+        end
+        render :text => "ok"
+     else
+        super
+      end
+    end
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   match '/service_check', :to => "rails_service_check/checks#all"
+  match '/service_check/:action', :controller => "rails_service_check/checks"
 end
